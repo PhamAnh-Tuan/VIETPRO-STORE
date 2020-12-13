1ec90eecb8323a4e177bd945e4b19d789ac32932
VanHuyChu <vanhuyutehy@gmail.com>
2020-12-13 20:02:36 +0700

    Route Group vanhuy

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('login', 'LoginController@LoginGet')->name('login.get');
 Route::post('/login', 'LoginController@LoginPost');
 
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('', 'AdminController@index')->name('admin.index');
+    Route::group(['prefix' => 'sản-phẩm', 'namespace'=>'Product'], function () {
+        Route::get('', 'ProductController@index')->name('product.index');
+        Route::get('Thêm-mới-sản-phẩm.html', 'ProductController@create')->name('product.create');
+        Route::get('Chỉnh-sửa-chi-tiết-sản-phẩm.html', 'ProductController@edit')->name('product.edit');
+        Route::get('Xóa-sản-phẩm', 'ProductController@delete')->name('product.delete');
+    });
+});
