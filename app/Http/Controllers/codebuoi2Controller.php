1dc9183f773fb506eb3cff6e6da5269d404dc59c
VanHuyChu <vanhuyutehy@gmail.com>
2020-12-15 21:49:30 +0700

    code b2

---
+++ app/Http/Controllers/codebuoi2Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class codebuoi2Controller extends Controller
+{
+    public function FunctionName()
+    {
+        return "day la code buoi2";
+    }
+}
